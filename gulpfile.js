4157de11c999de5743ad9ef3e4ecd232a4902221
Xio9 <efremenkov1999@gmail.com>
2020-05-05 19:54:06 +0300

    gulp+browser.sync

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task('hello', function(done) {
+    console.log('Привет бичара!');
+    done();
+} );
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
